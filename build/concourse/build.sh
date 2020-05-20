4f0442507d40b1ba076f68a683cca0c8c013306e
Toshiaki Maki <tmaki@pivotal.io>
2020-05-20 15:15:28 +0900

    Use vendir instead of helm fetch

--- build/concourse/build.sh
+++ build/concourse/build.sh
@@ -4,10 +4,14 @@ set -eu
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "Generating concourse resource definitions..."
+# sub-chart
+ln -s "$(dirname ${SCRIPT_DIR})/postgresql/_vendir/stable" "${SCRIPT_DIR}/_vendir/charts"
 
-helm template concourse --namespace=concourse "${SCRIPT_DIR}/concourse" \
+helm template concourse --namespace=concourse "${SCRIPT_DIR}/_vendir" \
         --values="${SCRIPT_DIR}/default-values.yml" |
   ytt --ignore-unknown-comments -f - |
   kbld -f - \
   > "${SCRIPT_DIR}/../../config/_ytt_lib/concourse/rendered.yml"
 #  > "${SCRIPT_DIR}/../../demo.yml"
+
+rm -f "${SCRIPT_DIR}/_vendir/charts"
--- build/concourse/fetch-concourse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-rm -rf concourse
-helm fetch \
-    --repo https://concourse-charts.storage.googleapis.com \
-    --untar concourse \
-    --version 10.2.0
-rm -rf concourse-*.tgz
